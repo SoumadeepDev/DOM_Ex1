5e413f98c2b0d0a2414ad848c4aafafde5f62040
SoumadeepDev <chat.soumadeep@gmail.com>
2023-09-30 12:11:25 +0530

    Used getElementsByClassName,TagName as per tasks

--- script.js
+++ script.js
@@ -24,3 +24,29 @@ items[2].style.backgroundColor = "rgb(0,200,0)";
 var AddItems = document.querySelector(".container #main .title");
 AddItems.style.fontWeight = "900";
 AddItems.style.color = "rgb(0, 200, 0)";
+
+//new task from here ...
+
+// Creating a new element with different className
+const newItem = document.createElement("li");
+newItem.innerHTML = "New Item";
+newItem.className = "list-newItem";
+
+// Remove dot style from newItem
+newItem.style.listStyleType = "none";
+newItem.style.paddingLeft = "0";
+newItem.style.marginLeft = "0";
+newItem.style.borderLeft = "none";
+newItem.style.borderRadius = "0";
+
+const ul = document.getElementsByTagName("ul")[0]; //because this returns an array so we use first index to access it
+ul.appendChild(newItem);
+
+newItem_className = document.getElementsByClassName("list-newItem")[0];
+
+// Add the same styles to the newItem as other li elements
+newItem_className.style.backgroundColor = "#f4f4f4";
+newItem_className.style.fontWeight = "bold";
+newItem_className.style.textAlign = "left";
+newItem_className.style.padding = "10px 15px";
+newItem_className.style.border = "1px solid #dbdbdb";
